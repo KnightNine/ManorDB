f034f41045ca7e328196e39f2221c37b2632907a
KnightNine <34152090+KnightNine@users.noreply.github.com>
2020-05-12 23:59:34 -0700

    Update AssemblyInfo.cs

--- FDB/WindowsFormsApp1/Properties/AssemblyInfo.cs
+++ FDB/WindowsFormsApp1/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 //██║     ╚██████╔╝██║  ██║   ██║   ██║  ██║███████╗███████║███████║    ██████╔╝██████╔╝
 //╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝    ╚═════╝ ╚═════╝ 
 //======================================================================================
-//   Version: 1.1.6.0     Author: Alexander Trosok     
+//   Version: 1.2.6.0     Author: Alexander Trosok     
 //======================================================================================
 
 // General Information about an assembly is controlled through the following
